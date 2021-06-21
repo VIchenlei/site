7a6b390ddd1f7f3015fdf9e6b4117dfc662a48fa
chenlei <cl@yaxt#123cl@yaxt#123>
2021-06-21 16:35:27 +0800

    初始

---
+++ js/jquery.ReloadInDiv.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('#AJAX-AREA1').load('PRODUCT-DEPARTMENT.html #tittle');
+    $('#AJAX-AREA2').load('PRODUCT-DEPARTMENT.html #KnowMore');
+    $('#AJAX-AREA3').load('PRODUCT-DEPARTMENT.html #DETAILS');
+})
+
+/*形式都一样，用一个进行实验就好了*/
+
+/* $(document).ready(function(){
+    $("#SOFTWARE-DEPARTMENT").click(function(){
+    $('#AJAX-AREA1').load('SOFTWARE-DEPARTMENT.html #tittle');
+  })
+  $("#SOFTWARE-DEPARTMENT").click(function(){
+    $('#AJAX-AREA2').load('SOFTWARE-DEPARTMENT.html #KnowMore');
+  })
+  $("#SOFTWARE-DEPARTMENT").click(function(){
+    $('#AJAX-AREA').load('SOFTWARE-DEPARTMENT.html');
+  })
+})
+
+$(document).ready(function(){
+  $("#HARDWARE-DEPARTMENT").click(function(){
+    $('#AJAX-AREA').load('HARDWARE-DEPARTMENT.html');
+  })
+})
+
+$(document).ready(function(){
+  $("#FEATURES-BTN").click(function(){
+    $('#AJAX-AREA').load('FEATURES.html');
+  })
+})
+
+$(document).ready(function(){
+  $("#SOLUTIONS-BTN").click(function(){
+    $('#AJAX-AREA').load('SOLUTIONS.html');
+  })
+}) */
